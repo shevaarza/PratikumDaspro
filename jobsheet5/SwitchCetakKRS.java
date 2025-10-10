a9be2f15b555f14ea8f54e43be144cee1ca54dbe
shevaarza <shevaarza@gmail.com>
2025-10-11 00:32:31 +0700

    Attempt 2

---
+++ jobsheet5/SwitchCetakKRS.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class SwitchCetakKRS {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("===== Print KRS siakad =====");
+        System.out.print("Enter current Semester : ");
+        int Semester = input.nextInt();
+
+        switch (Semester) {
+            case 1:
+                System.out.println("KRS semester 1 is Displayed ");
+                break;
+            case 2:
+                System.out.println(" KRS Semester 2 is Diplayed ");
+                break;
+            case 3:
+                System.out.println("KRS semester 3 is Displayed ");
+                break;
+            case 4:
+                System.out.println("KRS semester 4 is Displayed ");
+                break;
+            case 5:
+                System.out.println("KRS semester 5 is Displayed ");
+                break;
+            case 6:
+                System.out.println("KRS semester 6 is Displayed ");
+                break;
+
+            default:
+            System.out.println(" Invalid Semester ");
+                break;
+        }
+    }
+}
