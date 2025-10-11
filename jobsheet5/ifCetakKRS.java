67a08c18a1a79a0fd7643531a91bcf0d16da4a5e
shevaarza <shevaarza@gmail.com>
2025-10-11 20:49:30 +0700

    Taks 1

--- jobsheet5/ifCetakKRS.java
+++ jobsheet5/ifCetakKRS.java
@@ -6,16 +6,24 @@ public class ifCetakKRS{
         
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("====== Print KRS siakad ======");
+        boolean loop = true;
+while (loop) {
+     System.out.println("====== Print KRS siakad ======");
         System.out.print(" Have the tuition  fees been paid in full> ( True / False ): ");
-        boolean uktlunas = sc.nextBoolean();
+        String uktlunas = sc.nextLine();
 
-        if(uktlunas){
+        if(uktlunas.equalsIgnoreCase("True")){
             System.out.println("UKT paymant has been verified !");
             System.out.println(" KRS now can be printed and you can ask the academic advisor to sign in");
-        }else {
+            loop = false;
+        }else if ( uktlunas.equalsIgnoreCase("False")){
             System.out.println("Registation rejected. Please pay UKT first ");
-        }
+            loop = false;
+        } else {
+            System.out.println("System error please try again ");
+        }         
+}
+       
        
 
     }
