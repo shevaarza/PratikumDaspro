dd5d261a2256763d6e05aa4c8d7b3223e9ce3b96
shevaarza <shevaarza@gmail.com>
2025-10-11 21:03:03 +0700

    Taks 2

---
+++ jobsheet5/ifelsemodulus.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ifelsemodulus{
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.println(" ===== progam check number even or odd ======");
+        System.out.print(" Enter the number :");
+        number = input.nextInt();
+
+        if(number % 2 == 0){
+            System.out.println("===== the result ======");
+            System.out.print(" the number is even !!");
+        }else{
+            System.out.println("===== The result =====");
+            System.out.println("The number is Odd !!");
+        }
+    }
+}
