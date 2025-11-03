552de45f250c6a08010bcadfa080d23f185cdd2d
shevaarza <shevaarza@gmail.com>
2025-11-03 13:31:31 +0700

    experiment 1

---
+++ Jobsheet8/Star.java
@@ -0,0 +1,17 @@
+package Jobsheet8;
+import java.util.Scanner;
+
+public class Star {
+    public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter the value of N :");
+    int n = scanner.nextInt();
+
+    for(int i = 1 ; i <= n ; i++){
+        System.out.print("*");
+    }
+        
+    }
+}
