0a939bb4c801b6d33a318f4489b81959fdd62087
shevaarza <shevaarza@gmail.com>
2025-11-04 09:40:06 +0700

    Experiment 1

---
+++ Jobsheet9/ArrayNumber24.java
@@ -0,0 +1,18 @@
+package Jobsheet9;
+
+public class ArrayNumber24 {
+   
+    public static void main(String[] args) {
+        
+        int[] number = new int[4];
+        number[0]=4;
+        number[1]=6;
+        number[2]=7;
+        number[3]=9;
+
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+        System.out.println(number[3]);
+    }
+}
