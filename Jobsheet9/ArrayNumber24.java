409858e2344ba44917f8459fe82ff6dc45023b5a
shevaarza <shevaarza@gmail.com>
2025-11-04 09:42:14 +0700

    Jobsheet 9 question 1.1

--- Jobsheet9/ArrayNumber24.java
+++ Jobsheet9/ArrayNumber24.java
@@ -4,11 +4,11 @@ public class ArrayNumber24 {
    
     public static void main(String[] args) {
         
-        int[] number = new int[4];
-        number[0]=4;
-        number[1]=6;
-        number[2]=7;
-        number[3]=9;
+        double[] number = new double[4];
+        number[0]=434.43;
+        number[1]=232.32;
+        number[2]=0.23121;
+        number[3]=9.231;
 
         System.out.println(number[0]);
         System.out.println(number[1]);
