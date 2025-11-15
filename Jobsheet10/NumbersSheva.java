607ab297a18326160df159c847486335192c3584
shevaarza <shevaarza@gmail.com>
2025-11-15 20:03:12 +0700

    experiment 3

---
+++ Jobsheet10/NumbersSheva.java
@@ -0,0 +1,13 @@
+package Jobsheet10;
+
+public class NumbersSheva {
+    
+    public static void main(String[] args) {
+        
+        String [][] mynumber = new String[3][];
+
+        mynumber[0] = new String[5];
+        mynumber[1] = new String[3];
+        mynumber[2] = new String[1];
+    }
+}
