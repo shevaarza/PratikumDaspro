7f59bcbe6fb1d3deb900d5757c3fb72be6fd56de
shevaarza <shevaarza@gmail.com>
2025-11-15 11:43:57 +0700

    experiment 1.12

--- Jobsheet10/CinemaSheva.java
+++ Jobsheet10/CinemaSheva.java
@@ -19,6 +19,34 @@ public class CinemaSheva {
         System.out.printf("%-10s %-10s%n", audience[1][0], audience[1][1]);
         System.out.printf("%-10s %-10s%n", audience[2][0], audience[2][1]);
         System.out.printf("%-10s %-10s%n", audience[3][0], audience[3][1]); 
+
+
+        System.out.println(audience[0].length);
+        System.out.println(audience[1].length);
+        System.out.println(audience[2].length);
+        System.out.println(audience[2].length);
+
+
+        System.out.println(audience.length);
+        for(int i = 0 ; i < audience.length ; i++){
+            System.out.println("Length of audience row " + i + " : " + audience[i].length);
+        }
+
+
+        for(String[] row : audience){
+            System.out.println("Length of row : " + row.length);
+        }
+
+
+        System.out.println("audience in the row 3 : ");
+        for(int i = 0 ; i < audience[2].length;i++){
+            System.out.println(audience[2][i]);
+        }
+        
+
+        for(int i = 0 ; i < audience.length ; i++){
+            System.out.println("Audience in row " + (i +1) + String.join(", " , audience[i]));
+        }
     }
 }
 
@@ -33,6 +61,32 @@ public class CinemaSheva {
 // 2. Why is there a null in the list of audience names?
 // answer:
 // The null in the list of audience names indicates that the corresponding array element has not been assigned a value.
+
+
+// 4. Explain the function of audience.length and audience[0].length! 
+// Do audience[0].length, audience[1].length, audience[2].length, and 
+// audience[3].length have the same value? Why?
+// answer:
+// audience.length returns the number of rows in the 2D array, which is 4
+
+
+// 7. In your opinion, what are the advantages and disadvantages of foreach loop compared 
+// to for loop? 
+// answer:
+// Advantages of foreach loop:
+// 1. Simplicity: Foreach loops are easier to read and write, especially when iterating over collections or arrays.
+// 2. Reduced Errors: Foreach loops eliminate the risk of off-by-one errors since you don't need to manage the loop counter manually.
+
+// 8. What is the max row index for the audience array? 
+// answer:
+// The max row index for the audience array is 3, since there are 4 rows
+
+// 9. What is the max column index for the audience array? 
+// answer:
+// The max column index for the audience array is 1, since there are 2 columns
+
+
+// 13. What is the function of String.join()?   
 
 
 
